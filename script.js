e167d5ec3b594a8e8e1c9a924e85e854b0272c16
KKumarCoder <krishnakumar.snm004@gmail.com>
2024-02-28 04:04:11 +0530

    implemenRandom

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 console.log("hiii ,,,,,")
+<<<<<<< Updated upstream
 console.log ("krishna kumar")
+=======
+function generationRafterAll(async(close) => {
+  server.close(() => {
+    close()
+   console.log('server closed');
+  })
+}
+>>>>>>> Stashed changes
